1f097bc09713bdac9d7d09ea4f17d4a6cfdd1f97
Satya <satyakra@buffalo.edu>
2017-11-04 16:59:21 -0400

    Shortened URL and PUT request for volume control.

--- app/src/main/java/com/prtify/android/services/SpotifyApi.java
+++ app/src/main/java/com/prtify/android/services/SpotifyApi.java
@@ -6,6 +6,7 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 /**
  * Created by leshan on 11/4/17.
@@ -18,14 +19,15 @@ import retrofit2.http.PUT;
  */
 public interface SpotifyApi {
 
-    @GET("https://api.spotify.com/v1/me/player/devices")
+    @GET("/v1/me/player/devices")
     Call<DeviceResponse> getDevices(@Header("Authorization") String accessToken);
 
-    @PUT("https://api.spotify.com/v1/me/player/play")
+    @PUT("/v1/me/player/play")
     Call<Void> playTrack(@Header("Authorization") String accessToken);
 
-    @PUT("https://api.spotify.com/v1/me/player/pause")
+    @PUT("/v1/me/player/pause")
     Call<Void> pauseTrack(@Header("Authorization") String accessToken);
 
-
+    @PUT("/v1/me/player/volume?volume_percent={volumeValue}")
+    Call<Void> setVolume(@Header("Authorization") String accessToken, @Path("volumeValue") int volumeValue);
 }
